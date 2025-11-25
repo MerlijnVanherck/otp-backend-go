9dfa3e7e4870cf8b25415d58fd06e027a02a7dda
Merlijn Vanherck <merlijn.vanherck@bignited.be>
2025-11-25 08:29:09 +0100

    add nix dev shell

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs@{ flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      
+      perSystem =
+        { pkgs, ... }:
+        {
+          formatter = pkgs.nixfmt-tree;
+          
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              nil
+              
+              go
+              gopls
+              delve
+            ];
+          };
+        };
+    };
+}
